a44e1bdbb8ce750559c25d6fbefd949954034e1a
id774 <idnanashi@gmail.com>
2012-10-09 10:12:13 +0900

    4 Uncontaminated loadpath

--- lib/kmeans.rb
+++ lib/kmeans.rb
@@ -3,12 +3,7 @@
 
 module Kmeans
   VERSION = "0.0.2"
-  ROOT_DIR = File.expand_path("..", File.dirname(__FILE__))
-  $:.unshift ROOT_DIR
-  $:.unshift ROOT_DIR + '/lib'
-  $:.unshift ROOT_DIR + '/lib/kmeans'
-
-  require 'pair'
-  require 'pearson'
-  require 'cluster'
+  require File.dirname(__FILE__) +  "/kmeans/pair"
+  require File.dirname(__FILE__) +  "/kmeans/pearson"
+  require File.dirname(__FILE__) +  "/kmeans/cluster"
 end

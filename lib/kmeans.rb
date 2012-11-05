7d33c8005bd819bf62425b6e5d397f703808717c
id774 <idnanashi@gmail.com>
2012-11-05 11:06:36 +0900

    16 Version bump to 0.0.7

--- lib/kmeans.rb
+++ lib/kmeans.rb
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 module Kmeans
-  VERSION = "0.0.6"
+  VERSION = "0.0.7"
   require File.dirname(__FILE__) +  "/kmeans/pair"
   require File.dirname(__FILE__) +  "/kmeans/pearson"
   require File.dirname(__FILE__) +  "/kmeans/cluster"

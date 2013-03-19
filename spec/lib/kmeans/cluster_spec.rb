a1330983f25b187896a99d861dcc9456466a4a55
id774 <idnanashi@gmail.com>
2013-03-19 15:03:56 +0900

    20 Append test case for nil

--- spec/lib/kmeans/cluster_spec.rb
+++ spec/lib/kmeans/cluster_spec.rb
@@ -102,6 +102,39 @@ describe Kmeans::Cluster do
          "piyo"=>"1"
         }
       }
+
+    @porous_hash = {
+      "test01"=>
+        {"hoge"=>"0",
+         "fuga"=>"1",
+         "piyo"=>nil
+        },
+      "test02"=>
+        {"hoge"=>"2",
+         "fuga"=>"1",
+         "piyo"=>"3"
+        },
+      "test03"=>
+        {"hoge"=>"3",
+         "fuga"=>nil,
+         "piyo"=>"1"
+        },
+      "test04"=>
+        {"hoge"=>nil,
+         "fuga"=>"2",
+         "piyo"=>nil
+        },
+      "test05"=>
+        {"hoge"=>"4",
+         "fuga"=>"2",
+         "piyo"=>"3"
+        },
+      nil=>
+        {"hoge"=>"3",
+         "fuga"=>"1",
+         nil=>"1"
+        },
+      }
   end
 
   context 'の Cluster クラスにおいて' do
@@ -165,5 +198,24 @@ describe Kmeans::Cluster do
       end
     end
 
+    describe 'nil を含む二次元ハッシュを渡しても' do
+      it "Kmeans::Cluster クラスが返却される" do
+        result = Kmeans::Cluster.new(@porous_hash, {
+          :centroids => 5,
+          :loop_max => 10
+          })
+        result.class.should be_equal Kmeans::Cluster
+      end
+
+      it "ハッシュの配列が返却される (結果は実行ごとに異なる)" do
+        result = Kmeans::Cluster.new(@porous_hash, {
+          :centroids => 5,
+          :loop_max => 10
+          })
+        result.make_cluster
+        result.cluster.class.should be_equal Hash
+        result.cluster.values.class.should be_equal Array
+      end
+    end
   end
 end

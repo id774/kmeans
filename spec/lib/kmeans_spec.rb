aa52dc49cb3734aff5da27d4b4df690609cda673
id774 <idnanashi@gmail.com>
2013-07-09 16:47:36 +0900

    27 Version bump to 0.1.0

--- spec/lib/kmeans_spec.rb
+++ spec/lib/kmeans_spec.rb
@@ -5,7 +5,7 @@ require File.dirname(__FILE__) + '/../spec_helper'
 describe Kmeans do
   context 'のバージョンを参照した場合' do
     it "バージョンが正しく表示される" do
-      expect = '0.0.9'
+      expect = '0.1.0'
       Kmeans.const_get(:VERSION).should be_true
       Kmeans.const_get(:VERSION).should == expect
     end

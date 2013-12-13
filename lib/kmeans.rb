b2afe22a831f825e80f6e5ac95022b63696632ef
id774 <idnanashi@gmail.com>
2013-12-13 18:02:09 +0900

    41 Refactoring

--- lib/kmeans.rb
+++ lib/kmeans.rb
@@ -2,10 +2,10 @@
 # -*- coding: utf-8 -*-
 
 module Kmeans
-  VERSION = "0.1.1"
   require File.dirname(__FILE__) +  "/kmeans/pair"
   require File.dirname(__FILE__) +  "/kmeans/pearson"
   require File.dirname(__FILE__) +  "/kmeans/cluster"
   require File.dirname(__FILE__) +  "/kmeans/hcluster"
   require File.dirname(__FILE__) +  "/kmeans/dendrogram"
+  require File.dirname(__FILE__) +  "/kmeans/version"
 end
---
+++ lib/kmeans/version.rb
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+module Kmeans
+  VERSION = "0.1.1"
+end
